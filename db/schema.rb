a704c11bd8ff84262756bb1247de2d77bbff9cbf
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-03-10 15:45:03 +0530

    Name added to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207162049) do
+ActiveRecord::Schema.define(version: 20170310101044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170207162049) do
     t.string   "provider"
     t.string   "uid"
     t.string   "token"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
