b4e0fda2633090be5436eba8d38f9f2757628e39
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-04-05 23:57:15 +0530

    Condition for deleted from FB added

--- app/models/loop_task.rb
+++ app/models/loop_task.rb
@@ -7,7 +7,11 @@ class LoopTask
 			Post.scheduled.each do |post|
 				post.start if ((post.start_time.to_time < Time.now.getutc)  and (post.image != nil) and post.can_start?)
 			end
-			sleep(5)
+			Post.live.each do |post|
+				query = "https://graph.facebook.com/v2.8/?ids=#{post.video_id}&fields=reactions.type(LIKE).limit(0).summary(total_count).as(reactions_like),reactions.type(LOVE).limit(0).summary(total_count).as(reactions_love),reactions.type(WOW).limit(0).summary(total_count).as(reactions_wow),reactions.type(HAHA).limit(0).summary(total_count).as(reactions_haha),reactions.type(SAD).limit(0).summary(total_count).as(reactions_sad),reactions.type(ANGRY).limit(0).summary(total_count).as(reactions_angry)&access_token=#{post.user.token}"
+				post.stop("Deleted from FB") if HTTParty.get(query).parsed_response['data'].nil?
+			end
+			sleep(10)
 		end
 	end
 
