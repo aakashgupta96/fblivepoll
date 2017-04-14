fa0b4dbd408bb39b7725323d2de597e3b303a75d
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-04-15 02:56:51 +0530

    Fog bug solved

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,13 @@ CarrierWave.configure do |config|
 	if Rails.env.development? || Rails.env.test?
 		config.storage = :file
 	else
-		config.fog_provider = 'fog/aws'
-	  config.fog_credentials = {
+		config.fog_credentials = {
 	      :provider               => 'AWS',
 	      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
 	      :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
 	      :region                 => 'ap-south-1' # Change this for different AWS region. Default is 'us-east-1'
 	  }
+	  config.storage = :fog
 	  config.fog_directory  = "shurikenlive"
 	end
 
