478e706bb8b577b072ce7968ff7b9ee444002394
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-04-15 02:04:05 +0530

    Config for s3 changed only for production

--- app/models/update_frame.rb
+++ app/models/update_frame.rb
@@ -12,6 +12,8 @@ class UpdateFrame
     result = ERB.new(erb_str)
     result = result.result(namespace.instance_eval { binding })
 
+    path = File.join(Rails.root,'public','uploads','post',@post.id.to_s)
+    FileUtils.mkdir_p(path) unless File.exist?(path)
     File.open(html_file, 'w') do |f|
       f.write(result)
     end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-Redis.current = Redis.new(:host => "35.154.196.122", :port => 6379, :thread_safe => true, auth: "rasenshuriken")
-Resque.redis = Redis.current
+if Rails.env == "production"
+	Redis.current = Redis.new(:host => "35.154.196.122", :port => 6379, :thread_safe => true, auth: "rasenshuriken")
+	Resque.redis = Redis.current
+end
