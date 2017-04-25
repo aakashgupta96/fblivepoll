5f392a30f022acbd1657df347f5d8a21ac71881b
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-04-25 11:45:39 +0530

    Redis auth added

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
 	Redis.current = Redis.new(:host => ENV['HOST'], :port => ENV['POST'], :thread_safe => true, auth: ENV['AUTH'])
-	Redis.current.auht(ENV['AUTH'])
+	Redis.current.auth(ENV['AUTH'])
 	Resque.redis = Redis.current
 end
