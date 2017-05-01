f7cf47c55a9f2773c78aec8072aad77de8af7884
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-05-01 21:40:56 +0530

    Schedule post on basis of their id

--- app/models/loop_task.rb
+++ app/models/loop_task.rb
@@ -4,7 +4,7 @@ class LoopTask
 	def self.perform 
 	  loop do
 			#Code for Starting a scheduled post
-			Post.scheduled.each do |post|
+			Post.scheduled.order(:id).each do |post|
 				post.start if ((post.start_time.to_time < Time.now.getutc)  and (post.required_images_available?) and post.can_start?)
 			end	
 			sleep(10)
