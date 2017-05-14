dded78dc1d2a74f0699d58dc6e23920212248e60
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-05-14 21:35:35 +0530

    Time limit changed for polls and videos + Unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ working_directory app_dir
 # Set unicorn options
 worker_processes 2
 preload_app true
-timeout 240
+timeout 1200
 
 # Set up socket location
 listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
