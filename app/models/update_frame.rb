22d2c17e901590c70fb503a658201aabf075642a
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-06-26 23:36:33 +0530

    Direct link ank added to video template

--- app/models/update_frame.rb
+++ app/models/update_frame.rb
@@ -10,13 +10,13 @@ class UpdateFrame
     path = File.join(Rails.root,'public','uploads','post',@post.id.to_s)
     FileUtils.mkdir_p(path) unless File.exist?(path)
    
-    if @post.loop_video?
-      local_video_path = "#{Rails.root.to_s}/public/uploads/post/#{@post.id.to_s}"
-      if Rails.env.production?
-        %x[wget #{@post.video.url} -q -O #{local_video_path}/1.mp4]
-      end
-      video_path = "#{local_video_path}/1.mp4"
-    end
+    # if @post.loop_video?
+    #   local_video_path = "#{Rails.root.to_s}/public/uploads/post/#{@post.id.to_s}"
+    #   if Rails.env.production?
+    #     %x[wget #{@post.video.url} -q -O #{local_video_path}/1.mp4]
+    #   end
+    #   video_path = "#{local_video_path}/1.mp4"
+    # end
 
     if Rails.env.production?
       command = "$HOME/bin/ffmpeg -y -s 1280x720 -r 24 -f x11grab -i :99 -f alsa -i hw:0,1 -codec:a aac -ac 1 -ar 44100 -b:a 128k -preset ultrafast -filter:v 'crop=800:448:0:72' -vcodec libx264 -crf 23 -pix_fmt yuv420p -f flv '#{@post.key}' 2> #{Rails.root.join('log').join('stream').join(@post.id.to_s).to_s}"
@@ -70,9 +70,9 @@ class UpdateFrame
     %x[kill -9 #{ffmpeg_id}]
     driver.quit
     headless.destroy  
-    if @post.loop_video?
-      %x[rm -f #{video_path}]
-    end
+    # if @post.loop_video?
+    #   %x[rm -f #{video_path}]
+    # end
   end
 
 end
