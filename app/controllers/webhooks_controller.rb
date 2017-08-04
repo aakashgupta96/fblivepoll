e737467f5854a629c61810a926af575db2a21824
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-08-04 23:19:34 +0530

    Paypal client id changed

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,5 +2,6 @@ class WebhooksController < ApplicationController
   protect_from_forgery :except => :new_payment
   def new_payment
     puts params
+    render :nothing => true, :status => 200, :content_type => 'text/html'
   end
 end
