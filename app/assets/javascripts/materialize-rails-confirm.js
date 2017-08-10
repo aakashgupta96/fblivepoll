046a6b8294da27b6cc191221cb90094d13b179b2
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-08-10 16:49:23 +0530

    jquery confirm added

--- app/assets/javascripts/materialize-rails-confirm.js
+++ app/assets/javascripts/materialize-rails-confirm.js
@@ -11,37 +11,32 @@ $(function() {
   $.rails.confirmed = function(link) {
     var temp = link.attr("data-confirm");
     link.removeAttr("data-confirm");
-    $("#glass1").remove();
-    $("#modal1000").remove()
     link.trigger("click.rails");
     link.attr("data-confirm",temp);
     return
   };
 
   return $.rails.showConfirmDialog = function(link) {
-    var html, message;
-    html = void 0;
-    glass = "<div id=\"glass1\" style=\" position:absolute; opacity: 0.5; background: black; width: 100%; height: 5000px; z-index: 100000000;\"></div>";
-    message = void 0;
-    message = link.attr("data-confirm");
-    html = "<div id=\"modal1000\" class=\"modal\" style=\"z-index: 100000003; display: block; opacity: 1; color: grey; transform: scaleX(1); top: 10%;\"> <div class=\"modal-content\"><h5>" + message + "</h5></div><div class=\"modal-footer\"><a class=\"modal-action modal-close waves-effect waves-red btn-flat close\">Cancel</a><a class=\"modal-action modal-close waves-effect waves-green btn-flat confirm\">YES</a></div></div>";
-    $("body").append(glass);
-    $("body").append(html);
-
-    $("#modal1000").modal({
-      complete: function() {
-        $("#glass1").remove();
-        return $("#modal1000").remove();
+    var message = link.attr("data-confirm");
+    console.log(message);
+    $.confirm({
+      useBootstrap: false,
+      theme: 'material',
+      title: 'Confirm!',
+      content: message,
+      buttons: {
+          confirm: {
+          	btnClass: "btn-dark",
+          	action: function () {
+          		return $.rails.confirmed(link);
+          	}
+        	},
+          cancel: {
+          	action: function () {
+              //Ignore
+          	}
+          }
       }
-    });
-
-    $("#modal1000 .close").on("click", function() {
-      $("#glass1").remove();
-      return $("#modal1000").remove();;
-    });
-
-    return $("#modal1000 .confirm").on("click", function() {
-      return $.rails.confirmed(link);
     });
   };
 });
