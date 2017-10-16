dbc43181f8f1fc1f78757222ed58d7ab2511d9aa
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-10-17 01:25:58 +0530

    added method to get big pages

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,46 @@ class AdminUser < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.get_big_pages_id follower_count
+		pages_array = Array.new
+		big_pages = Set.new
+		Post.all.pluck(:page_id).to_set.each do |page_id|
+			if pages_array.size < 49	
+				pages_array << page_id
+			else
+				query = "https://graph.facebook.com/v2.8/?ids=#{pages_array.join(',')}&fields=fan_count&access_token=#{ENV['FB_ACCESS_TOKEN']}"
+				pages_array.clear
+				begin
+					response = HTTParty.get(query)
+					if response.ok?
+						response.parsed_response.each do |page_id,value|
+							if value["fan_count"].to_i > follower_count
+								big_pages << page_id
+							end
+						end
+					end
+				rescue Exception => e
+					puts e.class, e.message
+				end
+			end
+		end
+		query = "https://graph.facebook.com/v2.8/?ids=#{pages_array.join(',')}&fields=fan_count&access_token=#{ENV['FB_ACCESS_TOKEN']}"
+		pages_array.clear
+		begin
+			response = HTTParty.get(query)
+			byebug
+			if response.ok?
+				response.parsed_response.each do |page_id,value|
+					if value["fan_count"].to_i > follower_count
+						big_pages << page_id
+					end
+				end
+			end
+		rescue Exception => e
+			puts e.class, e.message
+		end
+		return big_pages
+	end
+
 end
