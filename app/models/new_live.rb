3b48c20a0de47d3f8cafef1adaaf379b650d8329
Aakash Gupta <aakash.gupta96@yahoo.in>
2017-10-26 19:20:44 +0530

    removed mahesh from new live notifications

--- app/models/new_live.rb
+++ app/models/new_live.rb
@@ -2,7 +2,7 @@ class NewLive
 	@queue = :new_live
 	@support = ["support@shurikenlive.com"]
 	@admins = ["aakash@shurikenlive.com"]
-	@editors = ["fahadnasar579@gmail.com","vmahesh20@gmail.com"]
+	@editors = ["fahadnasar579@gmail.com"]
 
 	def self.perform(post_id=nil)
 		link = "www.facebook.com/#{Post.find_by_id(post_id).video_id}"
