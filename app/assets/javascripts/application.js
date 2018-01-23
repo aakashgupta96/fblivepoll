11169e9c77f131102964cdc1fcfe1f41895dde2e
Aakash Gupta <aakash.gupta96@yahoo.in>
2018-01-24 00:16:37 +0530

    fixed home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,23 +46,6 @@ $( document ).ready(function(){
         $('.carousel').carousel('prev');
     });
 
-    window.fbAsyncInit = function() {
-      FB._https = true;
-      FB.init({
-        appId      : '241493676272963',
-        xfbml      : true,
-        version    : 'v2.8'
-      });
-    };
-
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/all.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
     $('#question-form').on('submit',function(){
       return validatePhoneNumber($("#icon_telephone").val()) && validateEmailId($("#icon_email").val());
     });
