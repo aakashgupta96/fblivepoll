4fdc6d48bd84c22c2fdb7c22a1c4ebc71949676e
Aakash Gupta <aakash.gupta96@yahoo.in>
2018-01-30 15:04:24 +0530

    fixed bug in destroy errorneous post

--- app/models/destroy_erroneous_posts.rb
+++ app/models/destroy_erroneous_posts.rb
@@ -3,7 +3,9 @@ class DestroyErroneousPosts
 
 	def self.perform(args=nil)
 	  Post.drafted.where("created_at <= ?", Time.now - 6.hours).destroy_all
-	  Post.scheduled.where("created_at <= ?", Time.now - 6.hours).select{|p| p.live_streams.empty?}.destroy_all
+	  Post.scheduled.where("created_at <= ?", Time.now - 6.hours).each do |p|
+	   	p.destroy if p.live_streams.empty?
+	 	end
 	end
 
 end
