ebe3dbb4c772b3ad17e04f7f1a391ad270320f53
Aakash Gupta <aakash.gupta96@yahoo.in>
2018-02-09 01:19:40 +0530

    fixed position of chrome browser

--- app/models/post.rb
+++ app/models/post.rb
@@ -231,9 +231,9 @@ class Post < ActiveRecord::Base
    		#profile = Selenium::WebDriver::Firefox::Profile.new; profile["toolkit.telemetry.enabled"] = false;profile["toolkit.telemetry.prompted"] = 2;profile["toolkit.telemetry.rejected"] = true
    		options = Selenium::WebDriver::Firefox::Options.new #(profile: profile)
    		headless = Headless.new(dimensions: "1920x1200x24")
-   	else
-   		width = 1280
-   		height = 786
+   	else 
+   		width = 1290
+   		height = 800
    		options = Selenium::WebDriver::Chrome::Options.new
    		options.add_argument("--no-sandbox")
    		headless = Headless.new(dimensions: "1920x1200x24", display: rand(100))
@@ -250,7 +250,7 @@ class Post < ActiveRecord::Base
     end
     prefix = get_html_url_prefix(browser)
     driver.navigate.to "#{prefix}#{self.html.url}"
-    driver.manage.window.position = Selenium::WebDriver::Point.new(0,0)
+    driver.manage.window.position = Selenium::WebDriver::Point.new(22,0)
     driver.manage.window.size = Selenium::WebDriver::Dimension.new(width,height)
     %x[DISPLAY=':#{headless.display}' xdotool mousemove #{width+10} #{height+10}]
     [driver,headless]
